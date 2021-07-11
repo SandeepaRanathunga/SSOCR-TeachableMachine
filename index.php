26fa19b4af4ccc7d86a88746631990fe1dd00b61
Sandeepa Ranathunga <55539896+SandeepaRanathunga@users.noreply.github.com>
2021-07-11 13:53:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="">
-        <?php
-            for($count=0;$count<5;$count++):
-        ?>
-        <label for="name">Name</label>
-        <?php
-            echo "<input type='text' name={$count} id=''>";
-            endfor;
-        ?>
-    </form>
-</body>
-</html>
+<?php
+    require_once('index.html');
+?>
